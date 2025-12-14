c06bdc889889fe37b746175da0234b5849f973b4
mir sina banihashem <four.x.cpn@gmail.com>
2025-12-14 19:04:07 +0330

    Create Node.js

---
+++ api/photo text/Node.js
@@ -0,0 +1,9 @@
+import fetch from "node-fetch";
+import fs from "fs";
+
+const url = "https://photo-text.api-sina-free.workers.dev/Hello";
+
+fetch(url)
+  .then(res => res.arrayBuffer())
+  .then(buf => fs.writeFileSync("hello.png", Buffer.from(buf)))
+  .then(() => console.log("🖼 تصویر ذخیره شد"));
