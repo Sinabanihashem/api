6999c755b8ab993d6d8e65451578fbf0a66ef4d0
mir sina banihashem <four.x.cpn@gmail.com>
2025-12-14 19:05:26 +0330

    Update Node.js

--- api/photo text/Node.js
+++ api/photo text/Node.js
@@ -6,4 +6,4 @@ const url = "https://photo-text.api-sina-free.workers.dev/Hello";
 fetch(url)
   .then(res => res.arrayBuffer())
   .then(buf => fs.writeFileSync("hello.png", Buffer.from(buf)))
-  .then(() => console.log("🖼 تصویر ذخیره شد"));
+  .then(() => console.log("🖼 Image saved!"));
